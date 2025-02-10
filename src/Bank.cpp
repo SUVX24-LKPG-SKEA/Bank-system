708d8f98e04f769a3ce4a38d8b7d625f45c875a3
LindeborgLove <lindeborglove@gmail.com>
2025-02-10 10:22:02 +0100

    Fix layout

--- src/Bank.cpp
+++ src/Bank.cpp
@@ -4,7 +4,8 @@
  * @param id takes in id of the bankAccount
  * @param info a pointer to the bankAccount which is allocated to the heap.
  */
-void Bank::addAccount(int id, bankAccount* info) {
+void Bank::addAccount(int id, bankAccount* info) 
+{
     std::lock_guard<std::mutex> lock(this->mapMtx);
     account[id] = info;
 }
@@ -13,7 +14,8 @@ void Bank::addAccount(int id, bankAccount* info) {
  * @param id takes in id of the bankAccount
  * @returns A pointer to the account
  */
-bankAccount* Bank::getAccount(int id) {
+bankAccount* Bank::getAccount(int id) 
+{
     std::lock_guard<std::mutex> lock(this->mapMtx);
     return account.at(id);
 }
@@ -28,7 +30,8 @@ Bank::~Bank()
     }
 }
 
-const std::map<int, bankAccount*> Bank::getAccounts() const noexcept {
+const std::map<int, bankAccount*> Bank::getAccounts() const noexcept 
+{
     return this->account;
 }
 
